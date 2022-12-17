b54e7c3ce4df0a5e9ddece0a53149d7e4a3664d9
K0sPr0 <Kos.Pro531@gmail.com>
2022-12-17 19:21:43 +0100

    update

---
+++ src/instagram.js
@@ -0,0 +1,10 @@
+import INSTAGRAN_API_KEY from "./apikey.js";
+
+var userFeed = new Instafeed({
+    debug: true,
+    limit: 3,
+    template: '<div class="{{model.media_type}}"><a href="{{link}}" target="_blank"><img class="insta-img" title="{{caption}}" src="{{image}}" /></a></div>',
+    target: "instafeed-container",
+    accessToken: 'test'
+});
+userFeed.run();
